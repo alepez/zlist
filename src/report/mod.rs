d350baccac2bc9aefb374326d0a48786db1433e6
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-11-16 12:50:46 +0100

    Add last time to report

--- src/report/mod.rs
+++ src/report/mod.rs
@@ -1,28 +1,50 @@
 use crate::parser::{AutoSnapList, Period};
+use chrono::prelude::*;
 use std::{collections::BTreeMap, fmt::Display};
+
+#[derive(Debug, Default)]
+struct AutoSnapReportPeriodItem {
+    count: usize,
+    last: Option<DateTime<Utc>>,
+}
 
 #[derive(Debug)]
 struct AutoSnapReportItem {
     name: String,
-    hourly_count: usize,
-    daily_count: usize,
-    monthly_count: usize,
-    yearly_count: usize,
+    hourly: AutoSnapReportPeriodItem,
+    daily: AutoSnapReportPeriodItem,
+    monthly: AutoSnapReportPeriodItem,
+    yearly: AutoSnapReportPeriodItem,
 }
 
 impl AutoSnapReportItem {
     fn new(name: String) -> Self {
         Self {
             name,
-            hourly_count: 0,
-            daily_count: 0,
-            monthly_count: 0,
-            yearly_count: 0,
+            hourly: Default::default(),
+            daily: Default::default(),
+            monthly: Default::default(),
+            yearly: Default::default(),
         }
     }
 }
 
 pub struct AutoSnapReport(Vec<AutoSnapReportItem>);
+
+fn update_last_timestamp(last: &mut Option<DateTime<Utc>>, timestamp: DateTime<Utc>) {
+    if let Some(last_time) = last {
+        if timestamp > *last_time {
+            *last = Some(timestamp);
+        }
+    } else {
+        *last = Some(timestamp);
+    }
+}
+
+fn update_period_item(curr: &mut AutoSnapReportPeriodItem, timestamp: DateTime<Utc>) {
+    update_last_timestamp(&mut curr.last, timestamp);
+    curr.count += 1
+}
 
 impl From<AutoSnapList> for AutoSnapReport {
     fn from(list: AutoSnapList) -> Self {
@@ -32,11 +54,13 @@ impl From<AutoSnapList> for AutoSnapReport {
             let key = &item.name;
             count
                 .entry(key.into())
-                .and_modify(|curr| match item.period {
-                    Period::Hourly => curr.hourly_count += 1,
-                    Period::Daily => curr.daily_count += 1,
-                    Period::Monthly => curr.monthly_count += 1,
-                    Period::Yearly => curr.yearly_count += 1,
+                .and_modify(|curr| {
+                    match item.period {
+                        Period::Hourly => update_period_item(&mut curr.hourly, item.timestamp),
+                        Period::Daily => update_period_item(&mut curr.daily, item.timestamp),
+                        Period::Monthly => update_period_item(&mut curr.monthly, item.timestamp),
+                        Period::Yearly => update_period_item(&mut curr.yearly, item.timestamp),
+                    };
                 })
                 .or_insert_with(|| AutoSnapReportItem::new(key.to_string()));
         }
@@ -50,14 +74,25 @@ impl From<AutoSnapList> for AutoSnapReport {
 impl Display for AutoSnapReport {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for item in self.0.iter() {
+            let times = [
+                item.hourly.last,
+                item.daily.last,
+                item.monthly.last,
+                item.yearly.last,
+            ];
+
+            let last_time = times.iter().filter_map(|x| x.as_ref()).max();
+            let last_time = last_time.map(|x| x.to_string()).unwrap_or_default();
+
             writeln!(
                 f,
-                "{:60} h: {:<3}, d: {:<3}, m: {:<3}, y: {:<3}",
+                "{:60} h: {:<3}, d: {:<3}, m: {:<3}, y: {:<3} {}",
                 item.name,
-                item.hourly_count,
-                item.daily_count,
-                item.monthly_count,
-                item.yearly_count
+                item.hourly.count,
+                item.daily.count,
+                item.monthly.count,
+                item.yearly.count,
+                last_time
             )?;
         }
 
